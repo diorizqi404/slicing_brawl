283df47768e01f2e406ed2ebec0632135079b8f1
ini nama <rizqiii.dio@gmail.com>
2023-12-05 22:00:57 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ["'Roboto', sans-serif"],
+        'dm-sans': ["'DM Sans', sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
